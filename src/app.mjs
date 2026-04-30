fa7cd60ab233dcfeb6faeb21081b4dbca4efb615
fedecalvar <calvarfede01@gmail.com>
2026-04-30 12:57:33 -0300

    Integración del frontend y configuración de archivos estáticos

--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,30 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import productRoutes from './routes/productRoute.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 
-// rutas
+// Servir archivos estáticos desde la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas API
 app.use('/api/products', productRoutes);
 
+// Ruta raíz → sirve el index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
+
 export default app;
+
+
+
+
+
+//tuleperaconlapapaya
